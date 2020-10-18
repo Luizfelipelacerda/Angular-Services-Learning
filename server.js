70019b83bd1ad4e21f54ea9e3db3b0ccd871ba12
Luiz Lacerda <luizfelipe_lacerda@ymail.com>
2020-10-18 14:15:21 -0300

    make it better4

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const PORT = process.env.PORT || 8080;
 
 
 app.use(express.static(__dirname + '/dist/my-first-app'));
-""
 app.get('/*', (req, res)=>{
     res.sendFile(__dirname + '/dist/my-first-app/index.html');
 });
 
 app.listen(PORT, ()=>{
     console.log('servidor Iniciado na porta '+ PORT);
-    
+
 })

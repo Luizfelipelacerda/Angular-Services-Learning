536457ec01d71986d5fdcc8923388f2fa851d389
Luiz Lacerda <luizfelipe_lacerda@ymail.com>
2020-10-18 12:00:28 -0300

    second commit

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { AccountsService } from '../accounts.services';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent {
+
+  constructor(private logginService : LoggingService,
+    private accountsService: AccountsService){}
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountsService.addAccount(accountName, accountStatus);
+    this.logginService.logStatusChange(accountStatus);
+  }
+}
